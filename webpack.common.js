e4803e277877029c95bb2937114bdaf03c5b65a1
rashmil-1999 <damonsalvatore833@gmail.com>
2021-09-18 14:24:44 -0500

    add node-module splitting

--- webpack.common.js
+++ webpack.common.js
@@ -93,6 +93,17 @@ module.exports = {
         splitChunks: {
             chunks: 'all',
             cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                      // get the name. E.g. node_modules/packageName/not/this/part.js
+                      // or node_modules/packageName
+                      const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+          
+                      // npm package names are URL-safe, but some servers don't like @ symbols
+                      return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
                 // Create a commons chunk, which includes all code shared between entry points.
                 commons: {
                     name: 'commons',
