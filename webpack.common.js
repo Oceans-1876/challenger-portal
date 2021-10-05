6071a77b677a00bab7cdb8e9f739f4a758eebe62
rashmil-1999 <damonsalvatore833@gmail.com>
2021-10-05 14:25:43 -0500

    add linting github action

--- webpack.common.js
+++ webpack.common.js
@@ -99,7 +99,7 @@ module.exports = {
                       // get the name. E.g. node_modules/packageName/not/this/part.js
                       // or node_modules/packageName
                       const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-          
+                      
                       // npm package names are URL-safe, but some servers don't like @ symbols
                       return `npm.${packageName.replace('@', '')}`;
                     },
