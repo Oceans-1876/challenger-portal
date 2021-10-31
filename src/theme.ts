db8e529687debf06aa351ed7ca3c7c4bde3bda61
kaveh <ka7eh@pm.me>
2021-10-31 14:15:58 -0500

    Fix typography breakpoints warning

--- src/theme.ts
+++ src/theme.ts
@@ -22,27 +22,37 @@ export const theme = createTheme({
 
 theme.typography.h1 = {
     fontSize: '6rem',
-    [theme.breakpoints.down('md')]: '5.5rem'
+    [theme.breakpoints.down('md')]: {
+        fontSize: '5.5rem'
+    }
 };
 
 theme.typography.h2 = {
     fontSize: '3.75rem',
-    [theme.breakpoints.down('md')]: '3rem'
+    [theme.breakpoints.down('md')]: {
+        fontSize: '3rem'
+    }
 };
 
 theme.typography.h3 = {
     fontSize: '3rem',
-    [theme.breakpoints.down('md')]: '2.75rem'
+    [theme.breakpoints.down('md')]: {
+        fontSize: '2.75rem'
+    }
 };
 
 theme.typography.h4 = {
     fontSize: '2.125rem',
-    [theme.breakpoints.down('md')]: '2rem'
+    [theme.breakpoints.down('md')]: {
+        fontSize: '2rem'
+    }
 };
 
 theme.typography.h5 = {
     fontSize: '1.5rem',
-    [theme.breakpoints.down('md')]: '1.25rem'
+    [theme.breakpoints.down('md')]: {
+        fontSize: '1.25rem'
+    }
 };
 
 theme.typography.h6 = {
