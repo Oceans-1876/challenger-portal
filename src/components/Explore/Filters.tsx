001a5550acdbdef3b46b3c15052d92c6628d2a6f
rashmil-1999 <damonsalvatore833@gmail.com>
2021-11-18 12:56:31 -0600

    fix eslint error

--- src/components/Explore/Filters.tsx
+++ src/components/Explore/Filters.tsx
@@ -6,7 +6,7 @@ import TextField from '@mui/material/TextField';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { matchSorter } from 'match-sorter';
-import throttle from "lodash/throttle";
+import throttle from 'lodash/throttle';
 
 import { DataStateContext, DataActionDispatcherContext } from '../../store/contexts';
 import { themeOptions } from '../../theme';
@@ -19,13 +19,13 @@ interface Props {
 const Filters = ({ filterBarHeight }: Props) => {
     const dataActionDispatcher = React.useContext(DataActionDispatcherContext);
     const { stationsList, filteredStations, speciesOptions, filteredFAOAreas } = React.useContext(DataStateContext);
-    const [options, setOptions] = React.useState<SpeciesOptions[]>(speciesOptions)
-    const [inputVal, setInputVal] = React.useState<string>("")
+    const [options, setOptions] = React.useState<SpeciesOptions[]>(speciesOptions);
+    const [inputVal, setInputVal] = React.useState<string>('');
     const faoAreas = useFAOAreas();
 
     React.useEffect(() => {
-        setOptions(speciesOptions)
-    }, [speciesOptions])
+        setOptions(speciesOptions);
+    }, [speciesOptions]);
 
     return (
         <AppBar
@@ -54,11 +54,14 @@ const Filters = ({ filterBarHeight }: Props) => {
                             size="small"
                             multiple
                             limitTags={0}
-                            renderInput={(params) => <TextField 
-                                {...params} 
-                                variant="filled"
-                                label="Stations"
-                                placeholder="Select Stations" />}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    variant="filled"
+                                    label="Stations"
+                                    placeholder="Select Stations"
+                                />
+                            )}
                             options={stationsList.map((station) => station.name)}
                             getOptionLabel={(option) => `Station ${option}`}
                             renderTags={() => null}
@@ -80,14 +83,14 @@ const Filters = ({ filterBarHeight }: Props) => {
                             size="small"
                             multiple
                             limitTags={0}
-                            renderInput={
-                                (params) => 
-                                <TextField 
-                                    {...params} 
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
                                     variant="filled"
                                     label="FAO Areas"
-                                    placeholder="Select FAO Areas" />
-                            }
+                                    placeholder="Select FAO Areas"
+                                />
+                            )}
                             options={faoAreas}
                             getOptionLabel={(option: FAOArea) => `${option.name} (${option.code})`}
                             renderTags={() => null}
@@ -116,30 +119,25 @@ const Filters = ({ filterBarHeight }: Props) => {
                             size="small"
                             autoComplete
                             multiple
-                            renderInput={
-                                (params) => 
-                                <TextField 
-                                    {...params} 
-                                    variant="filled"
-                                    label="Species"
-                                    placeholder="Select Species" />
-                            }
+                            renderInput={(params) => (
+                                <TextField {...params} variant="filled" label="Species" placeholder="Select Species" />
+                            )}
                             options={options}
                             getOptionLabel={(option) => {
                                 return option.label;
                             }}
                             clearOnBlur
-                            filterOptions={(options) => matchSorter(options, inputVal, {keys: ['label']})}
+                            filterOptions={(options_inp) => matchSorter(options_inp, inputVal, { keys: ['label'] })}
                             renderTags={() => null}
                             onChange={(_e, selectedOption) => {
-                                let ids: string[] = selectedOption.map(sp => (sp as SpeciesOptions).id)
+                                const ids: string[] = selectedOption.map((sp) => (sp as SpeciesOptions).id);
                                 dataActionDispatcher({
                                     type: 'updateFilteredSpecies',
                                     species: ids
                                 });
                             }}
                             onInputChange={(_e, newVal) => {
-                                throttle(() => setInputVal(newVal), 1000)()
+                                throttle(() => setInputVal(newVal), 1000)();
                             }}
                         />
                     </Box>
