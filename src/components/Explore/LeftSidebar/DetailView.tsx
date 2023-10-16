24bb723b74f3749ce45e4bf2a63032c9f8215bb5
Wenqi He <hewenqi96@gmail.com>
2023-10-16 10:22:14 -0500

    fix event capturing bug

--- src/components/Explore/LeftSidebar/DetailView.tsx
+++ src/components/Explore/LeftSidebar/DetailView.tsx
@@ -58,7 +58,8 @@ const DetailView = () => {
                             bottom: 0,
                             left: 0,
                             right: 0,
-                            overflow: 'hidden'
+                            overflow: 'hidden',
+                            pointerEvents: 'none'
                         }}
                     >
                         <Box
@@ -66,7 +67,8 @@ const DetailView = () => {
                                 height: '100%',
                                 transform: `translateX(${selectedSpeciesDetails ? 0 : DETAIL_VIEW_WIDTH}px)`,
                                 opacity: selectedSpeciesDetails ? 1 : 0,
-                                transition: 'all 0.2s ease-in-out, opacity 0s'
+                                transition: 'all 0.2s ease-in-out, opacity 0s',
+                                pointerEvents: 'all'
                             }}
                         >
                             <SpeciesDetailView />
